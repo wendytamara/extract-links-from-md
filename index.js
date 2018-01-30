8fde5359807051c9b64f666335f039764f9fce21
NelidaSh <nelida.quispetacuri@gmail.com>
2018-01-29 19:02:15 -0500

    añadiendo objeto exports y reordenando carpetas

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
+const fs = require('fs');
 
-// con este modulo se puede leer un archivo de texto
+const markdownLinks = require('./src/markdown-links.js');
 
-var fs = require('fs');
+const strMd = fs.readFileSync('public/README.md').toString();
 
-fs.readFile(__dirname + '/README.md', function(error, content){
-    var contentReadme = content.toString();
+let links = markdownLinks(strMd);
 
-    // Expresion regular que comprueba el formato markdown de los links
-    const urlRegex = /(\[(.*?)\])|(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi; // (ignore case)la expresión completa no distinge entre mayúsculas y minúsculas(case-insensitive), (global search)busqueda global.
-
-      let resultUrl = contentReadme.match(urlRegex); // Retorna un array
-      let linksMarkdown = [];
-      // Iteración de todo el arreglo resultUrl y busca cada elemento para almacenarlo con push en linksMarkdown
-      for (let i = 0; i < resultUrl.length; i++) {
-        linksMarkdown.push(
-          {href: resultUrl[i + 1],
-            text: resultUrl[i] }
-        );
-        i++;
-      }
-    //   console.log(linksMarkdown);
-      console.log(JSON.stringify(linksMarkdown));
-
-
-
-})
+console.log(links);
