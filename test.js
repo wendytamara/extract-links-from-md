f75e6d49129efdca63b7aa66022df482e460c4a9
wendytamara <anacarlavegam@gmail.com>
2018-01-31 22:03:23 -0500

    corrigiendo funcionalidad de los test

--- test.js
+++ test.js
@@ -1,11 +1,20 @@
-const assert = require('assert');
-const markdownLinks = require('./src/markdown-links.js');
 
-describe('markdownLinks()', () => {
-  it('debería devolver true para demo/README.md', () => {
-    assert.equal(markdownLinks('demo/README.md'), true);
+var extend = require('./src/markdown-links.js');
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('prueba de operacones', function() {
+  it('prueba de conversion1', function() {
+    assert.equal(markdownLinks(' [labore](https://en.wiktionary.org/wiki/labore)[foo](http://foo.com)') ,
+      '[{"href":"https://en.wiktionary.org/wiki/labore","text":"[labore]"}]' );
   });
-  it('debería devolver false para index.js', () => {
-    assert.equal(markdownLinks('index.js'), false);
+  it('prueba de conversion2', function() {
+    assert.equal(markdownLinks('[dolore](https://en.wiktionary.org/wiki/dolore)') ,
+      '[{"href":"https://en.wiktionary.org/wiki/dolore","text":"[dolore]"}]');
+  });
+
+  it('prueba de conversion2', function() {
+    assert.equal(markdownLinks('[foo](http://foo.com)') ,
+      '[{"href":"http://foo.com","text":"[foo]"}]');
   });
 });
